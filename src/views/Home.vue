74b1c7474fa632ae92a9b2364b0eeedf6400c66d
ChristinaJancy <chri91eq@easv365.dk>
2020-05-18 09:41:28 +0200

    deleted useless stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,7 +52,7 @@
         </v-row>
       </section>
 
-      <section id="about-me">
+      <section id="about-us">
         <div class="py-12"></div>
 
         <v-container class="text-center">
@@ -203,23 +203,6 @@ export default {
 
   },
   data: () => ({
-  articles: [
-          {
-            src: 'https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-            title: 'Mobile first & Responsive',
-            text: 'Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum.',
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-            title: 'Think outside the box',
-            text: 'Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh.',
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80',
-            title: 'Small changes, big difference',
-            text: 'Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget.',
-          },
-        ],
         features: [
           {
             icon: 'mdi-account-group-outline',
