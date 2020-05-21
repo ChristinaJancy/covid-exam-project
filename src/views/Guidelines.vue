50184a6aef61326a597d24489d6d89eeae9a7713
ChristinaJancy <chri91eq@easv365.dk>
2020-05-21 12:48:39 +0200

    new icons nd guideline

--- src/views/Guidelines.vue
+++ src/views/Guidelines.vue
@@ -5,7 +5,7 @@
         :height="$vuetify.breakpoint.smAndDown ? 400 : 300"
         src="../assets/guidelines/background1.jpg"
       >
-        <v-container fill-height >
+        <v-container fill-height>
           <v-row class="mx-auto" style="margin: 0 auto; padding: 0 auto;">
             <v-col cols="12">
               <div class="text-center">
@@ -16,9 +16,7 @@
         </v-container>
       </v-parallax>
     </section>
-    <h3
-      class="pt-5 mx-10"
-    >{{$t('guidelines.intro')}}</h3>
+    <h3 class="pt-5 mx-10">{{$t('guidelines.intro')}}</h3>
     <section style="line-height:40px;">
       <v-row style="margin: 0 auto; padding: 0 auto;">
         <!-----------------------------
@@ -43,11 +41,11 @@
             {{$t('guidelines.guide1.context1')}}
             <br />
             <br />
-            <b class="primary--text"> {{$t('guidelines.guide1.context2')}}</b>
+            <b class="primary--text">{{$t('guidelines.guide1.context2')}}</b>
             <br />
           </p>
-          <div class="text-left mx-10" style="font-size: 20px;" >
-            <li> {{$t('guidelines.guide1.context3.li1')}}</li>
+          <div class="text-left mx-10" style="font-size: 20px;">
+            <li>{{$t('guidelines.guide1.context3.li1')}}</li>
             <li>{{$t('guidelines.guide1.context3.li2')}}</li>
             <li>{{$t('guidelines.guide1.context3.li3')}}</li>
             <li>{{$t('guidelines.guide1.context3.li4')}}</li>
@@ -71,7 +69,7 @@
         <v-col sm="12" md="6" class="my-auto">
           <p class="pt-5 mx-10 text-left" style="font-size: 20px;">
             {{$t('guidelines.guide2.context1')}}
-            {{$t('guidelines.guide2.context2')}}   
+            {{$t('guidelines.guide2.context2')}}
           </p>
         </v-col>
 
@@ -98,28 +96,70 @@
             <br />When someone
             <b class="primary--text">coughs, sneezes, or speaks</b> they spray small liquid droplets from their nose or mouth which may contain virus.
             <br />
+            <br />If you are too close, you can breathe in the droplets, including the COVID-19 virus if the person has the disease.
+          </p>
+        </v-col>
+
+        <!---------------------
+
+    4. if feeling symptoms, stay home and isolate
+
+        --------------------->
+        <v-col sm="12" md="6">
+          <h1 class="my-12 pt-10 primary--text" style="font-size:32px;">4. If feeling symptoms
+</h1>
+          <v-row dense>
+            <v-col cols="6">
+              <v-img
+                class="mx-auto orangefilter"
+                max-height="250"
+                contain
+                style="width:calc(100% - 80px);"
+                src="../assets/icons/coughing.png"
+              ></v-img>
+            </v-col>
+            <v-col cols="6">
+              <v-img
+                class="mx-auto orangefilter"
+                max-height="250"
+                contain
+                style="width:calc(100% - 80px);"
+                src="../assets/icons/headache.png"
+              ></v-img>
+            </v-col>
+            <v-col cols="12">
+              <v-img
+                class="mx-auto orangefilter"
+                max-height="250"
+                contain
+                style="width:calc(100% - 80px);"
+                src="../assets/icons/hightempeture.png"
+              ></v-img>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col sm="12" md="6" class="my-auto pt-5">
+          <p class=" mx-10 text-left" style="font-size: 20px;">
+           Stay home and self-isolate even with minor symptoms such as 
+<br><br>
+            <b class="primary--text">Cough, headache and mild fever </b> until you recover.
             <br />
-If you are too close, you can breathe in the droplets, including the COVID-19 virus if the person has the disease.
+            <br />
+             Have someone bring you supplies. If you need to leave your house, wear a mask to avoid infecting others. 
+              <br />
+             Why? Avoiding contact with others will protect them from possible COVID-19 and other viruses.
           </p>
         </v-col>
       </v-row>
     </section>
-
-    <!-------------------------------------- 
-
-Posters 
-
---------------------------------------
-
-    -->
   </div>
 </template>
 
 
 <style lang="scss" scoped>
-.orangefilter{
-        filter: invert(41%) sepia(100%) saturate(5295%) hue-rotate(22deg)
-          brightness(100%) contrast(101%);
+.orangefilter {
+  filter: invert(41%) sepia(100%) saturate(5295%) hue-rotate(22deg)
+    brightness(100%) contrast(101%);
 }
 
 @media screen and (max-width: 700px) {
