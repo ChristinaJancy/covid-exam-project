4cf31243cf5ae6f6929c99bebda981b09e1ae04e
ChristinaJancy <chri91eq@easv365.dk>
2020-05-23 23:25:38 +0200

    update banner

--- src/views/Guidelines.vue
+++ src/views/Guidelines.vue
@@ -2,16 +2,27 @@
   <div id="guidelines" class="about text-center">
     <section>
       <v-parallax
-        :height="$vuetify.breakpoint.smAndDown ? 400 : 300"
+        :height="$vuetify.breakpoint.smAndDown ? 300 : 300"
         src="../assets/home/virusblue2.jpg"
       >
         <v-container fill-height>
           <v-row class="mx-auto" style="margin: 0 auto; padding: 0 auto;">
-            <v-col cols="12">
+            <v-col cols="12" align="center" class="my-auto mx-auto banner-img">
+              <v-img
+                class="png-shadow banner-img"
+                style="margin-top:-10px"
+                contain
+                width="auto"
+                max-width="250"
+                src="../assets/icons/guidelines-alone.png"
+              ></v-img>
+            </v-col>
+
+            <v-col cols="12" align="center">
               <div class="text-center">
                 <span
                   :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
-                  class="font-weight-light"
+                  class="font-weight-light banner-text"
                 >
                   <h1>{{$t('guidelines.title')}}</h1>
                 </span>
@@ -21,6 +32,7 @@
         </v-container>
       </v-parallax>
     </section>
+    <br />
     <h3 class="pt-5 mx-10">{{$t('guidelines.intro')}}</h3>
     <section style="line-height:40px;">
       <v-row style="margin: 0 auto; padding: 0 auto;">
@@ -33,13 +45,7 @@
           <h1 class="my-12 primary--text" style="font-size:32px;">{{$t('guidelines.guide1.title')}}</h1>
         </v-col>
         <v-col sm="12" md="6">
-          <v-img
-            max-height="350"
-            contain
-            class="mx-auto"
-            style="width:calc(100% - 80px);"
-            src="../assets/icons/washinghands.png"
-          ></v-img>
+          <v-img max-height="350" contain class src="../assets/icons/washinghands.png"></v-img>
         </v-col>
 
         <v-col md="6" sm="12" class="my-auto">
@@ -195,10 +201,11 @@
             {{$t('guidelines.guide5.context3')}}
             <br />
             <br />
-            {{$t('guidelines.guide5.context4')}}  <a
-          href="mailto:educovid@outlook.com"
-          class="primary--text font-weight-bold"
-        >educovid@outlook.com</a>
+            {{$t('guidelines.guide5.context4')}}
+            <a
+              href="mailto:educovid@outlook.com"
+              class="primary--text font-weight-bold"
+            >educovid@outlook.com</a>
           </p>
         </v-col>
       </v-row>
@@ -217,5 +224,14 @@
   #guidelines {
     margin-top: -104px;
   }
+  .banner-img {
+    display: none;
+  }
+}
+
+.png-shadow {
+  filter: url(#drop-shadow);
+  -webkit-filter: drop-shadow(3px 1px 10px rgba(0, 0, 0));
+  filter: progid:DXImageTransform.Microsoft.Dropshadow(OffX=12, OffY=12, Color='#444');
 }
 </style>
--- src/views/Rules.vue
+++ src/views/Rules.vue
@@ -2,22 +2,23 @@
   <div id="rules" class="about text-center">
     <section>
       <v-parallax
-        :height="$vuetify.breakpoint.smAndDown ? 400 : 300"
+        :height="$vuetify.breakpoint.smAndDown ? 300 : 300"
         src="../assets/home/virusblue2.jpg"
       >
         <v-container fill-height>
           <v-row class="mx-auto" style="margin: 0 auto; padding: 0 auto;">
-            <v-col cols="12" align="center" class="my-auto mx-auto">
+            <v-col cols="12" align="center" class="my-auto mx-auto banner-img">
                   <v-img
-                  class="my-5"
+                  class="my-5 png-shadow banner-img"
                   contain
                   width="auto"
                   max-width="300"
-                  style="filter: url(#drop-shadow); -webkit-filter: drop-shadow(3px 1px 10px rgba(0,0,0)); filter:progid:DXImageTransform.Microsoft.Dropshadow(OffX=12, OffY=12, Color='#444');"
                   src="../assets/icons/rules-alone.png"
                 ></v-img> 
-               
-               <!--  <div class="text-center">
+              
+            </v-col>
+            <v-col cols="12" align="center">
+                   <div class="text-center">
                 <span
                   :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
                   class="font-weight-light"
@@ -25,7 +26,7 @@
                   <h1>{{$t('rules.title')}}</h1>
                 </span>
               
-              </div>  -->
+              </div> 
             </v-col>
           </v-row>
         </v-container>
@@ -133,5 +134,13 @@
   #rules {
     margin-top: -104px;
   }
+  .banner-img{
+    display:none;
+  }
+}
+
+.png-shadow{
+  filter: url(#drop-shadow); -webkit-filter: drop-shadow(3px 1px 10px rgba(0,0,0)); 
+  filter:progid:DXImageTransform.Microsoft.Dropshadow(OffX=12, OffY=12, Color='#444');
 }
 </style>
