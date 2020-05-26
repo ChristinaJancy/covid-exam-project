1358523571a8623bd501ad9cafe4d9e170ce74ab
ChristinaJancy <chri91eq@easv365.dk>
2020-05-26 11:35:27 +0200

    Update Rules.vue

--- src/views/Rules.vue
+++ src/views/Rules.vue
@@ -136,47 +136,81 @@ Posters
 
           <v-tab href="#tab-2">Danish</v-tab>
         </v-tabs>
-      <v-col cols="12">
-        <h1 class="my-12 primary--text">
-          To help you remember the rules and make sure they are kept,
-          <br />these poster will be hanging around the educational institutions.
-          <br />
-        </h1>
-      </v-col>
+        <v-col cols="12">
+          <h1 class="my-12 primary--text">
+            To help you remember the rules and make sure they are kept,
+            <br />these poster will be hanging around the educational institutions.
+            <br />
+          </h1>
+        </v-col>
         <v-tabs-items v-model="tab">
-           <!-- English posters -->
+          <!-- English posters -->
           <v-tab-item value="tab-1">
             <v-card flat>
-              <v-row>
+              <v-row class="pa-0 ma-0">
                 <v-col md="4" sm="12">
+                  <h1 class="primary--text">KEEP DISTANCE</h1>
+                  <v-responsive class="mx-auto" width="56">
+                    <v-divider class="mb-1"></v-divider>
+                    <v-divider></v-divider>
+                  </v-responsive>
                   <v-img
                     max-height="500"
                     contain
-                    class="mx-auto"
+                    class="mx-auto poster"
                     style="width:calc(100% - 80px);"
                     src="../assets/rules/cafeteria-poster.png"
                   ></v-img>
-                  <h1 class="primary--text">KEEP DISTANCE</h1>
+                  <div>
+                    <a
+                      class
+                      href="https://firebasestorage.googleapis.com/v0/b/portfolio-708bc.appspot.com/o/posterdesign-main.png?alt=media&token=19d5db26-d81e-46fe-8452-66022c251a25"
+                      target="_blank"
+                    >View larger (opens new tab) </a>
+                  </div>
                 </v-col>
                 <v-col md="4" sm="12">
+                  <h1 class="primary--text">WASH YOUR HANDS</h1>
+                  <v-responsive class="mx-auto" width="56">
+                    <v-divider class="mb-1"></v-divider>
+                    <v-divider></v-divider>
+                  </v-responsive>
                   <v-img
                     max-height="500"
                     contain
-                    class="mx-auto"
+                    class="mx-auto poster"
                     style="width:calc(100% - 80px);"
                     src="../assets/rules/bathroom-poster.png"
                   ></v-img>
-                  <h1 class="primary--text">WASH YOUR HANDS</h1>
+                  <div>
+                    <a
+                      class
+                      href="https://firebasestorage.googleapis.com/v0/b/portfolio-708bc.appspot.com/o/bathroom-poster.png?alt=media&token=b015e532-d3f9-40db-8642-3e10028ee515"
+                      target="_blank"
+                    >View larger (opens new tab)</a>
+                  </div>
                 </v-col>
                 <v-col md="4" sm="12">
+                  <h1 class="primary--text">STAY SAFE</h1>
+                  <v-responsive class="mx-auto" width="56">
+                    <v-divider class="mb-1"></v-divider>
+                    <v-divider></v-divider>
+                  </v-responsive>
                   <v-img
                     max-height="500"
                     contain
-                    class="mx-auto"
+                    class="mx-auto poster"
                     style="width:calc(100% - 80px);"
                     src="../assets/rules/posterdesign-main.png"
                   ></v-img>
-                  <h1 class="primary--text">STAY SAFE</h1>
+                  <!--in the div = class="pa-0 ma-0 mx-12 pl-4" align="left"-->
+                  <div >
+                    <a
+                      class
+                      href="https://firebasestorage.googleapis.com/v0/b/portfolio-708bc.appspot.com/o/posterdesign-main.png?alt=media&token=19d5db26-d81e-46fe-8452-66022c251a25"
+                      target="_blank"
+                    >View larger (opens new tab)</a>
+                  </div>
                 </v-col>
               </v-row>
             </v-card>
@@ -187,34 +221,61 @@ Posters
             <v-card flat>
               <v-row>
                 <v-col md="4" sm="12">
+                  <h1 class="primary--text">HOLD FYSISK AFSTAND</h1>
+                  <v-responsive class="mx-auto" width="56">
+                    <v-divider class="mb-1"></v-divider>
+                    <v-divider></v-divider>
+                  </v-responsive>
                   <v-img
                     max-height="500"
                     contain
-                    class="mx-auto"
+                    class="mx-auto poster"
                     style="width:calc(100% - 80px);"
                     src="../assets/rules/cafeteria-posterDAN.png"
                   ></v-img>
-                  <h1 class="primary--text">HOLD FYSISK AFSTAND</h1>
+                   <a
+                      class
+                      href="https://firebasestorage.googleapis.com/v0/b/portfolio-708bc.appspot.com/o/cafeteria-posterDAN.png?alt=media&token=da426986-6f53-4ac7-827d-89adb5557541"
+                      target="_blank"
+                    >Se større (åbner ny fane)</a>
                 </v-col>
                 <v-col md="4" sm="12">
+                  <h1 class="primary--text">VASK DINE HÆNDER</h1>
+                  <v-responsive class="mx-auto" width="56">
+                    <v-divider class="mb-1"></v-divider>
+                    <v-divider></v-divider>
+                  </v-responsive>
                   <v-img
                     max-height="500"
                     contain
-                    class="mx-auto"
+                    class="mx-auto poster"
                     style="width:calc(100% - 80px);"
                     src="../assets/rules/bathroom-posterDAN.png"
                   ></v-img>
-                  <h1 class="primary--text">VASK DINE HÆNDER</h1>
+                      <a
+                      class
+                      href="https://firebasestorage.googleapis.com/v0/b/portfolio-708bc.appspot.com/o/bathroom-posterDAN.png?alt=media&token=1b63e7bc-0213-4711-94f8-5ecc6d4136bb"
+                      target="_blank"
+                    >Se større (åbner ny fane)</a>
                 </v-col>
                 <v-col md="4" sm="12">
+                  <h1 class="primary--text">PAS PÅ SIG SELV</h1>
+                  <v-responsive class="mx-auto" width="56">
+                    <v-divider class="mb-1"></v-divider>
+                    <v-divider></v-divider>
+                  </v-responsive>
                   <v-img
                     max-height="500"
                     contain
-                    class="mx-auto"
+                    class="mx-auto poster"
                     style="width:calc(100% - 80px);"
                     src="../assets/rules/posterdesign-mainDAN.png"
                   ></v-img>
-                  <h1 class="primary--text">PAS PÅ SIG SELV</h1>
+                      <a
+                      class
+                      href="https://firebasestorage.googleapis.com/v0/b/portfolio-708bc.appspot.com/o/posterdesign-mainDAN.png?alt=media&token=9df08b92-9da3-48b2-9bba-bc9ca92956da"
+                      target="_blank"
+                    >Se større (åbner ny fane)</a>
                 </v-col>
               </v-row>
             </v-card>
